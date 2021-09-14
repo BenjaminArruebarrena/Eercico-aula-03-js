69415237936806914e051d05ca1a5c45d3870269
Benjamin Arruebarrena <arruebarrena15@gmail.com>
2021-09-14 11:21:24 -0300

    exercicio da aula 03 js

---
+++ aula03.js
@@ -0,0 +1,8 @@
+const nome = prompt('Digite seu primeiro nome')
+    console.log(nome);
+
+const sobrenome = prompt('digite se Sobenome')
+    console.log(sobrenome);
+
+const iddade = prompt('Digite sua Idade')
+    console.log(iddade)
